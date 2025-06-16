1dd5788f9e45d441b871782733aae1f42ea2c5ac
SteveAllan25 <jasonalando2@gmail.com>
2025-06-16 11:19:37 +0000

    some changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "polls",
+    "polls.apps.PollsConfig",  #  Only this line is needed
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -121,3 +122,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.app.github.dev",  # This allows GitHub Codespace URLs
+]
+
